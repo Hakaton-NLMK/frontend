87509576b9cbd09876b0df90a31e6c98aee93196
Fedor Pushkov <fedor.pushkov@bimeister.com>
2024-09-10 20:17:57 +0300

    Migrated project to Typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Hero from "./hero/Hero";
+import Navbar from "./navbar/Navbar";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
